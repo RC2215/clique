86e0af77529f6abc62cad432d778aebacc9b3c74
RC2215 <88962047+RC2215@users.noreply.github.com>
2026-05-31 00:41:10 +0300

    WIP

--- clique/utils.py
+++ clique/utils.py
@@ -1,13 +1,10 @@
 import copy
-import logging
 from collections.abc import Callable
-from functools import partial, update_wrapper, wraps
-from os import PathLike
+from functools import partial, update_wrapper
 from typing import Any, Final
 
 from click import Command, Option, make_pass_decorator
 
-from .core import CliqueGroup
 from .exceptions import CliqueException
 from .leader import Leader
 
@@ -25,25 +22,6 @@ def _partial(func: Callable[..., Any], *args: Any, **kwargs: Any) -> Callable[..
     return update_wrapper(partial(func, *args, **kwargs), func)
 
 
-def _set_group_attr(attr_name, attr_value):
-    def inner(func):
-        if not isinstance(func, CliqueGroup):
-            raise CliqueException('Operation not supported on non CliqueGroup object')
-        setattr(func, attr_name, attr_value)
-        return func
-
-    return inner
-
-
-def set_logger(default_log_level: int = logging.INFO, file_path: str | PathLike[str] | None = None) -> None:
-    pass
-
-
-# set_message_template - a meta key? pass_meta_key
-set_message_templates = partial(_set_group_attr, 'message_templates')
-# set_logger = partial(_set_group_attr, 'default_log_level')  # ? only boolean? no, with option to log_level!
-
-
 def make_sub_command(name: str, cmd: Command, default_map: dict[str, Any], force: bool = True) -> Command:
     """
     TBD: Docstring
@@ -52,12 +30,11 @@ def make_sub_command(name: str, cmd: Command, default_map: dict[str, Any], force
     new_command.name = name.lower().replace('_', '-')
     overridden_params: dict[str, Any] = {}
 
-    for param in new_command.params.copy():
+    for param in new_command.params:
         value = default_map.get(param.name, NOTHING)
         if value is not NOTHING:
             default_map.pop(param.name)
             if force:
-                new_command.params.remove(param)
                 overridden_params[param.name] = value
             else:
                 param.default = value
@@ -69,6 +46,7 @@ def make_sub_command(name: str, cmd: Command, default_map: dict[str, Any], force
 
     if overridden_params:
         assert new_command.callback is not None
+        new_command.params = [param for param in new_command.params if param.name not in overridden_params]
         new_command.callback = _partial(new_command.callback, **overridden_params)
 
     return new_command
